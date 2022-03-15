942e6fb9eb3899b15d7e86ccbb94c486bcf483d2
Lucas Kuhn <hi@lucaskuhn.dev>
2022-03-15 15:19:55 -0300

    free entire exec args when command is not found

--- src/ft_exec.c
+++ src/ft_exec.c
@@ -6,53 +6,53 @@
 /*   By: lalex-ku <lalex-ku@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 15:26:54 by lalex-ku          #+#    #+#             */
-/*   Updated: 2022/03/15 15:12:45 by lalex-ku         ###   ########.fr       */
+/*   Updated: 2022/03/15 15:18:33 by lalex-ku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "limits.h"
 #include "pipex.h"
 
-char	*get_executable(char *cmd, char **envp);
+char	*get_exec_path(char **exec_args, char **envp);
 char	**get_paths(char **envp);
 
 void	ft_exec(char *command, char **envp)
 {
-	char	*path;
+	char	*exec_path;
 	char	**exec_args;
 
-	path = get_executable(command, envp);
 	exec_args = get_exec_args(command);
-	execve(path, exec_args, envp);
+	exec_path = get_exec_path(exec_args, envp);
+	execve(exec_path, exec_args, envp);
 	exit(errno);
 }
 
-char	*get_executable(char *cmd, char **envp)
+char	*get_exec_path(char **exec_args, char **envp)
 {
-	char	*executable;
+	char	*exec_path;
 	char	**paths;
 	int		i;
 
 	paths = get_paths(envp);
-	executable = malloc(sizeof(char) * (PATH_MAX));
+	exec_path = malloc(sizeof(char) * (PATH_MAX));
 	i = 0;
 	while (paths[i])
 	{
-		ft_strlcat(executable, paths[i], PATH_MAX);
-		ft_strlcat(executable, "/", PATH_MAX);
-		ft_strlcat(executable, cmd, PATH_MAX);
-		if (access(executable, X_OK) == 0)
+		ft_strlcat(exec_path, paths[i], PATH_MAX);
+		ft_strlcat(exec_path, "/", PATH_MAX);
+		ft_strlcat(exec_path, exec_args[0], PATH_MAX);
+		if (access(exec_path, X_OK) == 0)
 			break ;
-		*executable = 0;
+		*exec_path = 0;
 		i++;
 	}
-	if (executable && *executable)
-		return (executable);
-	ft_free_arr(paths);
-	free(executable);
+	if (exec_path && *exec_path)
+		return (exec_path);
 	write(STDERR_FILENO, "command not found: ", 19);
-	write(STDERR_FILENO, cmd, ft_strlen(cmd));
+	write(STDERR_FILENO, exec_args[0], ft_strlen(exec_args[0]));
 	write(STDERR_FILENO, "\n", 1);
+	ft_free_arr(paths);
+	ft_free_arr(exec_args);
 	exit(127);
 }
 
